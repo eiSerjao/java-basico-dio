b6b327510ee7206cf147abef4c29ecbecff2b2c6
paulosergio <paulosergiobarrosdesouza@gmail.com>
2024-04-05 21:59:38 -0300

    Exemplo na Declaração de Metodos e Variaveis com comentarios.

--- java-anatomia-de-classes/src/MinhaClasse.java
+++ java-anatomia-de-classes/src/MinhaClasse.java
@@ -1,19 +1,23 @@
 public class MinhaClasse {
   
   public static void main(String[] args) {
-    
-    //Declarando Variáveis e Métodos
-    String primeiroNome = "Paulo";
-    String segundoNome = "Sérgio";
+    // Método principal (ponto de entrada do programa)
 
+    // Declarando variáveis para os nomes
+    String primeiroNome = "Paulo"; // Primeiro nome
+    String segundoNome = "Sérgio"; // Segundo nome
+
+    // Chamando o método nomeCompleto para combinar os nomes e armazenar o resultado
     String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    // Imprimindo o nome completo na saída padrão
     System.out.println(nomeCompleto);
-  }
+}
 
-  public static String nomeCompleto (String primeiroNome, String segundoNome){
+// Método para combinar os nomes e retornar o nome completo
+public static String nomeCompleto(String primeiroNome, String segundoNome) {
+    // Concatenando os nomes e adicionando um prefixo
     return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
-
-
-  }
+}
 
 }
